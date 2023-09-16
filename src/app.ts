4dda48343e40bf376a90c5ff6e0f4ac86a39490d
Gerson Rocha <gersonrocha9@gmail.com>
2023-09-16 20:18:18 -0300

    ✨ feat: create user prisma migration + connect to docker db

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,24 @@
+import { PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
 
 export const app = fastify()
+export const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Gerson',
+    phone: '11999999999',
+    email: 'gersonrocha9@gmail.com',
+    password: '123456',
+    zipcode: '12345678',
+    street: 'Rua 1',
+    number: '123',
+    complement: 'Casa',
+    city: 'São Paulo',
+    uf: 'SP',
+    identifyProfile: 'PRODUCER',
+    mainAgriculturalActivity: 'CROPS',
+    hectaresCultivationArea: 100,
+    haveInternetDailyAccess: true,
+  },
+})
